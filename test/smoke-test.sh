734b7978785e30a19f3f0ab124685d33e2d2fd9a
Sasha Goldshtein <goldshtn@gmail.com>
2017-04-13 02:19:42 -0400

    Add smoke test
    The smoke test runs the Java target application, attaches the profiler,
    waits for 5 seconds, stops the profiler and dumps the results to a
    file, and then parses the file looking for patterns that we know
    must appear. This is a very basic smoke test that will only make sure
    we haven't broken anything completely obvious.
    
    The Makefile and Travis configuration are also updated to run the test.

---
+++ test/smoke-test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e  # exit on any failure
+set -x  # print all executed lines
+
+pushd $(dirname $0)
+
+javac Target.java
+java Target &
+
+FILENAME=/tmp/java.trace
+JAVAPID=$!
+
+sleep 1     # allow the Java runtime to initialize
+../profiler.sh -p $JAVAPID -a start
+sleep 5
+../profiler.sh -p $JAVAPID -a dump -f $FILENAME
+
+kill $JAVAPID
+
+function assert_string() {
+  if ! grep -q "$1" $FILENAME; then
+    exit 1
+  fi
+}
+
+assert_string "Target.main;Target.method1 "
+assert_string "Target.main;Target.method2 "
+assert_string "Target.main;Target.method3;java/util/Scanner"
+
+popd
