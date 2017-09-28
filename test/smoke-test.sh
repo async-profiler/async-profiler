74a7928f67d703756cdc33f8e4e117a149f35437
Dmitry Samersoff <dms@samersoff.net>
2017-09-28 20:55:53 +0300

    Minor fixes for smoke test

--- test/smoke-test.sh
+++ test/smoke-test.sh
@@ -3,27 +3,34 @@
 set -e  # exit on any failure
 set -x  # print all executed lines
 
-pushd $(dirname $0)
+if [ -z "${JAVA_HOME}" ]
+then
+  echo "JAVA_HOME is not set"
+fi
 
-javac Target.java
-java Target &
-
-FILENAME=/tmp/java.trace
-JAVAPID=$!
-
-sleep 1     # allow the Java runtime to initialize
-../profiler.sh -f $FILENAME -o collapsed -d 5 $JAVAPID
-
-kill $JAVAPID
-
-function assert_string() {
-  if ! grep -q "$1" $FILENAME; then
-    exit 1
+(
+  if [ "Target.class" -ot "Target.java" ]
+  then
+     ${JAVA_HOME}/bin/javac Target.java
   fi
-}
 
-assert_string "Target.main;Target.method1 "
-assert_string "Target.main;Target.method2 "
-assert_string "Target.main;Target.method3;java/util/Scanner"
+  ${JAVA_HOME}/bin/java Target &
 
-popd
+  FILENAME=/tmp/java.trace
+  JAVAPID=$!
+
+  sleep 1     # allow the Java runtime to initialize
+  ../profiler.sh -f $FILENAME -o collapsed -d 5 $JAVAPID
+
+  kill $JAVAPID
+
+  function assert_string() {
+    if ! grep -q "$1" $FILENAME; then
+      exit 1
+    fi
+  }
+
+  assert_string "Target.main;Target.method1 "
+  assert_string "Target.main;Target.method2 "
+  assert_string "Target.main;Target.method3;java/util/Scanner"
+)
