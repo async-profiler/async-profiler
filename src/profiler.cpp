053b0423c4ffcfc9de1f4c253646881cb101170f
Andrey Pangin <andrey.pangin@corp.mail.ru>
2018-07-17 01:44:54 +0300

    Check gcc compilation error

--- src/profiler.cpp
+++ src/profiler.cpp
@@ -425,7 +425,7 @@ Error Profiler::start(Arguments& args) {
     _frame_buffer = (ASGCT_CallFrame*)malloc(_frame_buffer_size * sizeof(ASGCT_CallFrame));
     _frame_buffer_index = 0;
     _frame_buffer_overflow = false;
-    _threads = args._threads && !args._dump_jfr;
+    __atomic_store_n(&_threads, args._threads && !args._dump_jfr, __ATOMIC_SEQ_CST);
 
     resetSymbols();
     initJvmtiFunctions();
