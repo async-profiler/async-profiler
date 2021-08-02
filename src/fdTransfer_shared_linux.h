17b71313534873adc5b4ad09f9e86e5cc155bf39
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2021-08-02 03:28:58 +0300

    Update copyrights

--- src/fdTransfer_shared_linux.h
+++ src/fdTransfer_shared_linux.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Yonatan Goldschmidt
+ * Copyright 2021 Andrei Pangin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
