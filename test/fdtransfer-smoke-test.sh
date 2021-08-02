cb5b6ded8b07ef6e7149b45a393e5cdc776a85d9
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2021-08-02 03:27:12 +0300

    Add sudo in tests (code runs as uid=1001)

--- test/fdtransfer-smoke-test.sh
+++ test/fdtransfer-smoke-test.sh
@@ -22,7 +22,7 @@ fi
   JAVAPID=$!
 
   sleep 1     # allow the Java runtime to initialize
-  ../profiler.sh -f $FILENAME -o collapsed -e cpu --fdtransfer -d 5 $JAVAPID
+  sudo ../profiler.sh -f $FILENAME -o collapsed -e cpu --fdtransfer -d 5 $JAVAPID
 
   kill $JAVAPID
 
