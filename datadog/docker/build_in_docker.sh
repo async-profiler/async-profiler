d738c47a818595dbbd4ca34b07bd0ac2785aed1e
Jaroslav Bachorik <jaroslav.bachorik@datadoghq.com>
2022-02-04 17:39:24 +0100

    Properly handle build failures

--- datadog/docker/build_in_docker.sh
+++ datadog/docker/build_in_docker.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
+set -euo pipefail
 
 export PROFILER_VERSION=$1
 FORCE_TESTS=$2
 
 cd /data/src/async-profiler
 # make sure all previously compiled classes are gone to prevent any bytecode incompatibility
-find . -name '*.class' | xargs -I {} rm -f {}
+find . -name '*.class' | xargs -I {} rm -f {} || true
 # build the library
 make clean all
 if [ "yes" = "$FORCE_TESTS" ]; then
--- datadog/scripts/build_locally.sh
+++ datadog/scripts/build_locally.sh
@@ -5,7 +5,7 @@ HERE=$(cd "${SCRIPT_DIR}" &>/dev/null && printf "%s/%s" "$PWD")
 ASYNC_PROFILER_DIR="${HERE}/../.."
 MAVEN_DIR="${HERE}/../maven"
 
-set -euo pipefail
+set -uo pipefail
 IFS=$'\n\t'
 
 # Helper functions
@@ -31,6 +31,9 @@ while getopts "a:tfh" arg; do
   case $arg in
     a)
       ARCH=${OPTARG}
+      if [ ${ARCH} = "linux-arm64" ]; then
+        PLATFORM="linux/arm64"
+      fi
       ;;
     f)
       FORCE_REBUILD="yes"
@@ -93,11 +96,17 @@ popd >> /dev/null
 
 echo "-> Building native library"
 # run the native build
-docker run --rm -it --platform linux/arm64 -v ${ASYNC_PROFILER_DIR}:/data/src/async-profiler -v ${MAVEN_DIR}/resources/native-libs/${ARCH}:/data/libs -v ${HERE}/../maven/repository:/root/.m2/repository ${TAG} /devtools/build_in_docker.sh ${VERSION} ${FORCE_TESTS} > /tmp/docker.log
+docker run --rm -it --platform ${PLATFORM} -v ${ASYNC_PROFILER_DIR}:/data/src/async-profiler -v ${MAVEN_DIR}/resources/native-libs/${ARCH}:/data/libs -v ${HERE}/../maven/repository:/root/.m2/repository ${TAG} /devtools/build_in_docker.sh ${VERSION} ${FORCE_TESTS} > /tmp/docker.log
 
-if [ 0 -ne $? ]; then
+if [ 0 -ne $? ] || [ ! -z "$(fgrep 'error' /tmp/docker.log)" ]; then
+  # print the output and exit if there is error
   cat /tmp/docker.log
   exit 1
+fi
+
+if [ ! -z "$(fgrep 'warning' /tmp/docker.log)" ]; then
+  # just print the output for warnings
+  cat /tmp/docker.log
 fi
 
 echo "-> Building maven artifact"
@@ -106,9 +115,16 @@ mkdir -p ${MAVEN_DIR}/tmp
 cp ${MAVEN_DIR}/pom.xml ${MAVEN_DIR}/tmp/pom.xml
 mvn -f ${MAVEN_DIR}/tmp/pom.xml versions:set -DnewVersion=${VERSION} > /dev/null
 mvn -DskipTests "-Dasync.profiler.dir=${ASYNC_PROFILER_DIR}" "-Dnative.resource.dir=${MAVEN_DIR}/resources" --no-transfer-progress -f ${MAVEN_DIR}/tmp/pom.xml clean package > /tmp/maven.log
-if [ 0 -ne $? ]; then
+
+if [ $? -ne 0 ] || [ ! -z "$(fgrep 'BUILD FAILURE' /tmp/maven.log)" ]; then
+  # print the output and exit if there is error
   cat /tmp/maven.log
   exit 1
+fi
+
+if [ -z "$(grep -iF 'warning' /tmp/maven.log)" ]; then
+  # just print the output for warnings
+  cat /tmp/maven.log
 fi
 
 echo "-> Build done : Artifacts available |"
