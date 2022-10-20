a44163c7cd14ed8bae4502fc78558153429bf111
Richard Startin <richard.startin@datadoghq.com>
2022-10-20 10:52:37 +0100

    add test for context values, fix endianness of buffer

--- test/maven/java/linkage/AsyncProfilerLinkageTest.java
+++ test/maven/java/linkage/AsyncProfilerLinkageTest.java
@@ -1,28 +1,17 @@
 package linkage;
 
-import java.io.File;
-import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
 
 import one.profiler.AsyncProfiler;
-import java.util.concurrent.ThreadLocalRandom;
+import utils.Utils;
 
 public class AsyncProfilerLinkageTest {
 
     @Test
     public void testContextApiLinked() throws Exception {
-        AsyncProfiler ap = AsyncProfiler.getInstance(getAsyncProfilerLib());
+        AsyncProfiler ap = AsyncProfiler.getInstance(Utils.getAsyncProfilerLib());
         ap.addThread(Thread.currentThread());
         ap.setContext(1, 1);
         ap.clearContext();
-    }
-
-    private static String getAsyncProfilerLib() throws Exception {
-        File root = new File(AsyncProfilerLinkageTest.class
-            .getResource("AsyncProfilerLinkageTest.class").toURI()).getParentFile();
-        while (!root.getName().equals("async-profiler")) {
-            root = root.getParentFile();
-        }
-        return root.toPath().resolve("build/libasyncProfiler.so").toAbsolutePath().toString();
     }
 }
