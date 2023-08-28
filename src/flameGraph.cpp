80802b6f7a543249ca71a87dd3b62eba196bf7a1
Olga Stogova <helgastogova@gmail.com>
2023-08-28 15:27:00 +0100

    change the three layout

--- src/flameGraph.cpp
+++ src/flameGraph.cpp
@@ -172,17 +172,17 @@ void FlameGraph::printTreeFrame(std::ostream& out, const Trie& f, int level) {
         StringUtils::replace(name, '<', "&lt;", 4);
         StringUtils::replace(name, '>', "&gt;", 4);
 
+        std::string divClass = trie->_children.size() > 0 ? "" : " class=\"o\"";
+        
         if (_reverse) {
             snprintf(_buf, sizeof(_buf) - 1,
-                     "<li><div>[%d] %.2f%% %s</div><span class=\"t%d\"> %s</span>\n",
-                     level,
-                     trie->_total * pct, Format().thousands(trie->_total),
+                     "<li><div%s>%.2f%% [%s]</div> <span class=\"t%d\">%s</span>\n",
+                     divClass.c_str(), trie->_total * pct, Format().thousands(trie->_total),
                      type, name.c_str());
         } else {
             snprintf(_buf, sizeof(_buf) - 1,
-                     "<li><div>[%d] %.2f%% %s self: %.2f%% %s</div><span class=\"t%d\"> %s</span>\n",
-                     level,
-                     trie->_total * pct, Format().thousands(trie->_total),
+                     "<li><div%s>%.2f%% [%s] &#8226; self: %.2f%% [%s]</div> <span class=\"t%d\">%s</span>\n",
+                     divClass.c_str(), trie->_total * pct, Format().thousands(trie->_total),
                      trie->_self * pct, Format().thousands(trie->_self),
                      type, name.c_str());
         }
