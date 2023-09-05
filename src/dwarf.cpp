56a50a8002bc9be005845612dc40467f4fa20a30
Andrei Pangin <noreply@pangin.pro>
2023-09-05 01:44:49 +0100

    Handle AArch64-specific DWARF opcode

--- src/dwarf.cpp
+++ src/dwarf.cpp
@@ -20,30 +20,31 @@
 
 
 enum {
-    DW_CFA_nop                = 0x0,
-    DW_CFA_set_loc            = 0x1,
-    DW_CFA_advance_loc1       = 0x2,
-    DW_CFA_advance_loc2       = 0x3,
-    DW_CFA_advance_loc4       = 0x4,
-    DW_CFA_offset_extended    = 0x5,
-    DW_CFA_restore_extended   = 0x6,
-    DW_CFA_undefined          = 0x7,
-    DW_CFA_same_value         = 0x8,
-    DW_CFA_register           = 0x9,
-    DW_CFA_remember_state     = 0xa,
-    DW_CFA_restore_state      = 0xb,
-    DW_CFA_def_cfa            = 0xc,
-    DW_CFA_def_cfa_register   = 0xd,
-    DW_CFA_def_cfa_offset     = 0xe,
-    DW_CFA_def_cfa_expression = 0xf,
-    DW_CFA_expression         = 0x10,
-    DW_CFA_offset_extended_sf = 0x11,
-    DW_CFA_def_cfa_sf         = 0x12,
-    DW_CFA_def_cfa_offset_sf  = 0x13,
-    DW_CFA_val_offset         = 0x14,
-    DW_CFA_val_offset_sf      = 0x15,
-    DW_CFA_val_expression     = 0x16,
-    DW_CFA_GNU_args_size      = 0x2e,
+    DW_CFA_nop                     = 0x0,
+    DW_CFA_set_loc                 = 0x1,
+    DW_CFA_advance_loc1            = 0x2,
+    DW_CFA_advance_loc2            = 0x3,
+    DW_CFA_advance_loc4            = 0x4,
+    DW_CFA_offset_extended         = 0x5,
+    DW_CFA_restore_extended        = 0x6,
+    DW_CFA_undefined               = 0x7,
+    DW_CFA_same_value              = 0x8,
+    DW_CFA_register                = 0x9,
+    DW_CFA_remember_state          = 0xa,
+    DW_CFA_restore_state           = 0xb,
+    DW_CFA_def_cfa                 = 0xc,
+    DW_CFA_def_cfa_register        = 0xd,
+    DW_CFA_def_cfa_offset          = 0xe,
+    DW_CFA_def_cfa_expression      = 0xf,
+    DW_CFA_expression              = 0x10,
+    DW_CFA_offset_extended_sf      = 0x11,
+    DW_CFA_def_cfa_sf              = 0x12,
+    DW_CFA_def_cfa_offset_sf       = 0x13,
+    DW_CFA_val_offset              = 0x14,
+    DW_CFA_val_offset_sf           = 0x15,
+    DW_CFA_val_expression          = 0x16,
+    DW_CFA_AARCH64_negate_ra_state = 0x2d,
+    DW_CFA_GNU_args_size           = 0x2e,
 
     DW_CFA_advance_loc        = 0x1,
     DW_CFA_offset             = 0x2,
@@ -249,6 +250,10 @@ void DwarfParser::parseInstructions(u32 loc, const char* end) {
                             _ptr += getLeb();
                         }
                         break;
+#ifdef __aarch64__
+                    case DW_CFA_AARCH64_negate_ra_state:
+                        break;
+#endif
                     case DW_CFA_GNU_args_size:
                         skipLeb();
                         break;
