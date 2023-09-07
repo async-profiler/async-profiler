6c35cc90283afc5b83878865b2fc81612fdeef2a
Michael Fan <Michael.Jingzhong.Fan@gmail.com>
2023-09-07 12:21:36 +0000

    rm builddir assertion wraps revert accidental delete profile printing

--- test/test/lock/LockTests.java
+++ test/test/lock/LockTests.java
@@ -9,7 +9,7 @@ public class LockTests {
     @Test(mainClass = DatagramTest.class, debugNonSafepoints = true)
     public void datagramSocketLock(TestProcess p) throws Exception {
         Output out = p.profile("-e cpu -d 3 -o collapsed --cstack dwarf"); 
-        assert (((out.ratio("Unsafe..ark") + out.ratio("Unsafe..npark")) > 0.1)||((out.ratio("PlatformEvent::.ark") + out.ratio("PlatformEvent::.npark")) > 0.1));
+        out.assertRatioGreater("(Unsafe..ark|Unsafe..npark|PlatformEvent::.ark|PlatformEvent::.npark)", 0.1);
 
         out = p.profile("-e lock -d 3 -o collapsed");
         assert out.samples("sun/nio/ch/DatagramChannelImpl.send") > 10;
