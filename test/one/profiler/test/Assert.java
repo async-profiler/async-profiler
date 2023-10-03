3219113cb387e5e93de29c5fc41fff805cd6d3bd
Michael Fan <Michael.Jingzhong.Fan@gmail.com>
2023-10-03 15:28:22 +0100

    tick conversion and cleanup

--- test/one/profiler/test/Assert.java
+++ test/one/profiler/test/Assert.java
@@ -24,27 +24,27 @@ public class Assert {
 
     public static void contains(Output out, String regex) throws AssertionError {
         if (!out.contains(regex)) {
-            throw new AssertionError("Expected: " + regex + "\n Received out may be available in build/test/logs.");
+            throw new AssertionError("Expected: " + regex + "\nReceived out may be available in build/test/logs.");
         }
     }
 
     public static void notContains(Output out, String regex) throws AssertionError {
         if (out.contains(regex)) {
-            throw new AssertionError("Expected not: " + regex + "\n Received out may be available in build/test/logs.");
+            throw new AssertionError("Expected not: " + regex + "\nReceived out may be available in build/test/logs.");
         }
     }
 
     public static void ratioGreater(Output out, String regex, double threshold) {
         double num = out.ratio(regex);
         if (num < threshold) {
-            throw new AssertionError("Expected " + regex + " ratio > " + threshold + "\ngot: " + num + "\n Received out may be available in build/test/logs.");
+            throw new AssertionError("Expected " + regex + " ratio > " + threshold + "\ngot: " + num + "\nReceived out may be available in build/test/logs.");
         }
     }
 
     public static void ratioLess(Output out, String regex, double threshold) {
         double num = out.ratio(regex);
         if (num > threshold) {
-            throw new AssertionError("Expected " + regex + " ratio < " + threshold + "\ngot: " + num + "\n Received out may be available in build/test/logs.");
+            throw new AssertionError("Expected " + regex + " ratio < " + threshold + "\ngot: " + num + "\nReceived out may be available in build/test/logs.");
         }
     }
 }
