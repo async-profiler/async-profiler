48a97b64e7a8b009d9d8a531a87c7194e94089c2
Thomas Matthijs <selckin@selckin.be>
2023-11-24 16:28:34 +0100

    #827: Handle both -jar and -cp when running converter.jar

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -14,13 +14,42 @@
  * limitations under the License.
  */
 
+import java.util.Arrays;
+
 /**
  * Main entry point of jar.
  * Lists available converters.
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            usage();
+            return;
+        }
+
+        String[] converterArgs = Arrays.copyOfRange(args, 1, args.length);
+        switch (args[0]) {
+            case "FlameGraph":
+                FlameGraph.main(converterArgs);
+                break;
+            case "jfr2flame":
+                jfr2flame.main(converterArgs);
+                break;
+            case "jfr2nflx":
+                jfr2nflx.main(converterArgs);
+                break;
+            case "jfr2pprof":
+                jfr2pprof.main(converterArgs);
+                break;
+            default:
+                System.out.println("Unknown converter: " + args[0] + "\n");
+                usage();
+                System.exit(1);
+        }
+    }
+
+    private static void usage() {
         System.out.println("Usage: java -cp converter.jar <Converter> [options] <input> <output>");
         System.out.println();
         System.out.println("Available converters:");
