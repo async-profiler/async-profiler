9fad48890ccd551b7da26b29185e81514064fe64
Andrei Pangin <noreply@pangin.pro>
2024-01-20 15:34:53 +0000

    Use shorter variant of copyright headers

--- src/launcher/main.cpp
+++ src/launcher/main.cpp
@@ -1,17 +1,6 @@
 /*
- * Copyright 2023 Andrei Pangin
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright The async-profiler authors
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <alloca.h>
@@ -35,8 +24,7 @@
 #define APP_BINARY "asprof"
 
 static const char VERSION_STRING[] =
-    "Async-profiler " PROFILER_VERSION " built on " __DATE__ "\n"
-    "Copyright 2016-2023 Andrei Pangin\n";
+    "Async-profiler " PROFILER_VERSION " built on " __DATE__ "\n";
 
 static const char USAGE_STRING[] =
     "Usage: " APP_BINARY " [action] [options] <pid>\n"
