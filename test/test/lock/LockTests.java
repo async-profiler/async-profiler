74ffc675be4006d6486a153dc1e48dcc6f96efe3
Andrei Pangin <noreply@pangin.pro>
2024-10-14 12:34:15 +0100

    Fix flaky tests

--- test/test/lock/LockTests.java
+++ test/test/lock/LockTests.java
@@ -18,7 +18,7 @@ public class LockTests {
         assert out.ratio("(PlatformEvent::.ark|PlatformEvent::.npark)") > 0.1
                 || (out.ratio("ReentrantLock.lock") > 0.1 && out.contains("Unsafe_.ark"));
         out = p.profile("-e lock -d 3 -o collapsed");
-        assert out.samples("sun/nio/ch/DatagramChannelImpl.send") > 10;
+        assert out.contains("sun/nio/ch/DatagramChannelImpl.send");
     }
 
     @Test(mainClass = RaceToLock.class, inputs = "0", output = true)
@@ -29,17 +29,19 @@ public class LockTests {
 
         Output out = p.profile("--lock " + interval + " --threads -o collapsed");
         Output stdout = p.readFile(TestProcess.STDOUT);
-        long shared1 = out.samples("\\[shared1");
-        long shared2 = out.samples("\\[shared2");
 
         Assert.isGreater(out.samples("\\[random1"), 0, "sampled all threads 1/4");
         Assert.isGreater(out.samples("\\[random2"), 0, "sampled all threads 2/4");
-        Assert.isGreater(shared1, 0, "sampled all threads 3/4");
-        Assert.isGreater(shared2, 0, "sampled all threads 4/4");
+        Assert.isGreater(out.samples("\\[shared1"), 0, "sampled all threads 3/4");
+        Assert.isGreater(out.samples("\\[shared2"), 0, "sampled all threads 4/4");
 
-        double sharedDiff = 1.0 * Math.abs(shared1 - shared2) / Math.max(shared1, shared2);
+        if (interval == 0) {
+            long shared1 = out.samples("\\[shared1");
+            long shared2 = out.samples("\\[shared2");
+            double sharedDiff = (double) Math.abs(shared1 - shared2) / Math.max(shared1, shared2);
+            Assert.isLess(sharedDiff, 0.1, "sharedDiff < 0.1");
+        }
 
-        Assert.isLess(sharedDiff, 0.15, "sharedDiff < 0.15");
         Assert.isGreater(stdout.samples("sharedWaitTime"), stdout.samples("randomWaitTime"), "sharedWaitTime > randomWaitTime");
     }
 }
