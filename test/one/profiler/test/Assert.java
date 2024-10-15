dff1095bdf7517bd7912499de1b869cca5f5805e
krk <keremkat@gmail.com>
2024-10-15 10:38:59 +0000

    Simplify how assertions are made and reported in Assert.java

--- test/one/profiler/test/Assert.java
+++ test/one/profiler/test/Assert.java
@@ -5,37 +5,76 @@
 
 package one.profiler.test;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiPredicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
+enum Comparison {
+    GT, GTE, LT, LTE,
+}
+
 public class Assert {
     private static final Logger log = Logger.getLogger(Assert.class.getName());
+    private static final Map<Comparison, String> operator = new HashMap<>();
+    private static final Map<Comparison, BiPredicate<Double, Double>> comparator = new HashMap<>();
 
-    public static void isGreater(double value, double threshold) {
-        isGreater(value, threshold, null);
+    static {
+        operator.put(Comparison.GT, ">");
+        operator.put(Comparison.GTE, ">=");
+        operator.put(Comparison.LT, "<");
+        operator.put(Comparison.LTE, "<=");
+
+        comparator.put(Comparison.GT, (a, b) -> a > b);
+        comparator.put(Comparison.GTE, (a, b) -> a >= b);
+        comparator.put(Comparison.LT, (a, b) -> a < b);
+        comparator.put(Comparison.LTE, (a, b) -> a <= b);
     }
 
-    public static void isGreater(double value, double threshold, String message) {
-        boolean asserted = value <= threshold;
-        log.log(Level.FINE, "isGreater (asserted: " + asserted + ") " + (message == null ? "" : message) + ": " + value + " > " + threshold);
+    private static void assertComparison(Comparison comparison, double left, double right, String message) {
+        boolean asserted = !comparator.get(comparison).test(left, right);
+        String operation = left + " " + operator.get(comparison) + " " + right;
+
+        log.log(Level.FINE, "isAsserted " + asserted + ", "
+                + (message == null ? "" : "'" + message + "'") + ": "
+                + operation);
 
         if (asserted) {
-            throw new AssertionError(
-                    "Expected " + value + " > " + threshold + (message != null ? (": " + message) : ""));
+            throw new AssertionError("Expected " + operation + (message != null ? (": " + message) : ""));
         }
     }
 
-    public static void isLess(double value, double threshold) {
-        isLess(value, threshold, null);
+    public static void isGreater(double left, double right) {
+        isGreater(left, right, null);
     }
 
-    public static void isLess(double value, double threshold, String message) {
-        boolean asserted = value >= threshold;
-        log.log(Level.FINE, "isLess (asserted: " + asserted + ")" + (message == null ? "" : message) + ": " + value + " < " + threshold);
+    public static void isGreater(double left, double right, String message) {
+        assertComparison(Comparison.GT, left, right, message);
+    }
 
-        if (asserted) {
-            throw new AssertionError(
-                    "Expected " + value + " < " + threshold + (message != null ? (": " + message) : ""));
-        }
+    public static void isGreaterOrEqual(double left, double right) {
+        isGreaterOrEqual(left, right, null);
+    }
+
+    public static void isGreaterOrEqual(double left, double right, String message) {
+        assertComparison(Comparison.GTE, left, right, message);
+    }
+
+    public static void isLess(double left, double right) {
+        isLess(left, right, null);
+    }
+
+    public static void isLess(double left, double right, String message) {
+        assertComparison(Comparison.LT, left, right, message);
+    }
+
+    public static void isLessOrEqual(double left, double right) {
+        isLessOrEqual(left, right, null);
+    }
+
+    public static void isLessOrEqual(double left, double right, String message) {
+        assertComparison(Comparison.LTE, left, right, message);
     }
 }
