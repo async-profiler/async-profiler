05ae87cb5222d359ac9aef8d5e1812edb62d510f
krk <keremkat@gmail.com>
2024-10-15 14:24:43 +0000

    address comments

--- test/one/profiler/test/Assert.java
+++ test/one/profiler/test/Assert.java
@@ -5,37 +5,33 @@
 
 package one.profiler.test;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.BiPredicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
-enum Comparison {
-    GT, GTE, LT, LTE,
-}
-
 public class Assert {
-    private static final Logger log = Logger.getLogger(Assert.class.getName());
-    private static final Map<Comparison, String> operator = new HashMap<>();
-    private static final Map<Comparison, BiPredicate<Double, Double>> comparator = new HashMap<>();
+    enum Comparison {
+        GT(">", (a, b) -> a > b),
+        GTE(">=", (a, b) -> a >= b),
+        LT("<", (a, b) -> a < b),
+        LTE("<=", (a, b) -> a <= b),
+        EQ("==", Double::equals),
+        NE("!=", (a, b) -> !Double.isNaN(a) && !Double.isNaN(b) && !a.equals(b));
 
-    static {
-        operator.put(Comparison.GT, ">");
-        operator.put(Comparison.GTE, ">=");
-        operator.put(Comparison.LT, "<");
-        operator.put(Comparison.LTE, "<=");
+        public final String operator;
+        public final BiPredicate<Double, Double> comparator;
 
-        comparator.put(Comparison.GT, (a, b) -> a > b);
-        comparator.put(Comparison.GTE, (a, b) -> a >= b);
-        comparator.put(Comparison.LT, (a, b) -> a < b);
-        comparator.put(Comparison.LTE, (a, b) -> a <= b);
+        private Comparison(String operator, BiPredicate<Double, Double> comparator) {
+            this.operator = operator;
+            this.comparator = comparator;
+        }
     }
 
+    private static final Logger log = Logger.getLogger(Assert.class.getName());
+
     private static void assertComparison(Comparison comparison, double left, double right, String message) {
-        boolean asserted = !comparator.get(comparison).test(left, right);
-        String operation = left + " " + operator.get(comparison) + " " + right;
+        boolean asserted = !comparison.comparator.test(left, right);
+        String operation = left + " " + comparison.operator + " " + right;
 
         log.log(Level.FINE, "isAsserted " + asserted + ", "
                 + (message == null ? "" : "'" + message + "'") + ": "
@@ -44,6 +40,22 @@ public class Assert {
         if (asserted) {
             throw new AssertionError("Expected " + operation + (message != null ? (": " + message) : ""));
         }
+    }
+
+    public static void isEqual(double left, double right) {
+        isEqual(left, right, null);
+    }
+
+    public static void isEqual(double left, double right, String message) {
+        assertComparison(Comparison.EQ, left, right, message);
+    }
+
+    public static void isNotEqual(double left, double right) {
+        isNotEqual(left, right, null);
+    }
+
+    public static void isNotEqual(double left, double right, String message) {
+        assertComparison(Comparison.NE, left, right, message);
     }
 
     public static void isGreater(double left, double right) {
