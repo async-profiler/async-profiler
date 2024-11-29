bc0aeb940dffb9064bac1b903819138a71d8f4d3
Andrei Pangin <noreply@pangin.pro>
2024-11-29 19:17:59 +0000

    Fixed test on macOS

--- test/test/cpu/CpuTests.java
+++ test/test/cpu/CpuTests.java
@@ -33,6 +33,6 @@ public class CpuTests {
         assertCloseTo(out.total(), 2_000_000_000, "itimer total should match profiling duration");
 
         out = p.profile("-d 2 -e itimer -i 1us --total -o collapsed");
-        Assert.isLess(out.total(), 5_000_000, "itimer does not support overrun");
+        Assert.isLess(out.total(), 1_000_000_000, "itimer does not support overrun");
     }
 }
