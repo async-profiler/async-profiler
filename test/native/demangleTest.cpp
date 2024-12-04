38bb8f659bc635687346afeeb8267575abc1daf4
Ariel Ben-Yehuda <arielby@amazon.com>
2024-12-04 16:40:32 +0000

    add more tests for native demangling

--- test/native/demangleTest.cpp
+++ test/native/demangleTest.cpp
@@ -61,6 +61,30 @@ TEST_CASE(Demangle_test_demangle_rust_v0) {
     free((void*)s);
 }
 
+TEST_CASE(Demangle_test_demangle_rust_v0_punycode) {
+    const char *s = Demangle::demangle("_RNqCs4fqI2P2rA04_11utf8_identsu30____7hkackfecea1cbdathfdh9hlq6y", false);
+    CHECK_EQ (strcmp(s,
+        "utf8_idents::\xe1\x83\xa1\xe1\x83\x90\xe1\x83\xad\xe1\x83\x9b\xe1\x83\x94\xe1\x83\x9a"
+        "\xe1\x83\x90\xe1\x83\x93_\xe1\x83\x92\xe1\x83\x94\xe1\x83\x9b\xe1\x83\xa0\xe1\x83\x98"
+        "\xe1\x83\x94\xe1\x83\x9a\xe1\x83\x98_\xe1\x83\xa1\xe1\x83\x90\xe1\x83\x93\xe1\x83\x98"
+        "\xe1\x83\x9a\xe1\x83\x98"), 0);
+    free((void*)s);
+}
+
+TEST_CASE(Demangle_test_demangle_rust_v0_const_string) {
+    const char *s = Demangle::demangle("_RIC0Kef09f908af09fa688f09fa686f09f90ae20c2a720f09f90b6f09f9192e29895f09f94a520c2a720f09fa7a1f09f929bf09f929af09f9299f09f929c_E", false);
+    CHECK_EQ(strcmp(s,
+        "::<{*\"\\u{1f40a}\\u{1f988}\\u{1f986}\\u{1f42e} \\u{a7} \\u{1f436}\\u{1f452}\\u{2615}\\u{1f525}"
+        " \\u{a7} \\u{1f9e1}\\u{1f49b}\\u{1f49a}\\u{1f499}\\u{1f49c}\"}>"), 0);
+    free((void*)s);
+}
+
+TEST_CASE(Demangle_test_demangle_rust_v0_invalid_backref) {
+    const char *s = Demangle::demangle("_RNvNvB0_1x1y", false);
+    CHECK_EQ(strcmp(s, "{invalid syntax}::x::y"), 0);
+    free((void*)s);
+}
+
 TEST_CASE(Demangle_test_demangle_rust_v0_expanding) {
     // Test that demangling of a symbol that is big is handled correctly
     const char *s = Demangle::demangle("_RNvMC0" "TTTTTT" "p" "Ba_E" "B9_E" "B8_E" "B7_E" "B6_E" "B5_E" "3run", false);
