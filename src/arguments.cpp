998e98610a7dd93543264182783eb80d510a2fbd
Sergey Melnikov <Melnikov.Sergey.V@gmail.com>
2025-02-05 09:13:11 +0000

    Wed  5 Feb 2025 09:13:11 UTC

--- src/arguments.cpp
+++ src/arguments.cpp
@@ -425,12 +425,12 @@ Error Arguments::parse(const char* args) {
             CASE("delayns")
                 _heartbit_delay_ns = strtol(value, NULL, 0);
 
-            CASE("clock-unix") {
+            CASE("clockunix") {
                 _heartbit_unix_clock = true;
                 _heartbit_realtime_clock = false;
             }
 
-            CASE("clock-realtime") {
+            CASE("clockrealtime") {
                 _heartbit_unix_clock = false;
                 _heartbit_realtime_clock = true;
             }
