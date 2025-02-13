5d51fd79c292f09edbb791e9343ad5de57a4dbff
Kerem Kat <keremkat@gmail.com>
2025-02-13 19:17:52 +0000

    Support posix_memalign and aligned_alloc in the native memory tracker (#1130)

--- test/test/nativemem/CallsAllNoLeak.java
+++ test/test/nativemem/CallsAllNoLeak.java
@@ -12,16 +12,37 @@ public class CallsAllNoLeak {
     private static final int MALLOC_SIZE = 1999993; // Prime size, useful in assertions.
     private static final int CALLOC_SIZE = 2000147;
     private static final int REALLOC_SIZE = 30000170;
+    private static final int POSIX_MEMALIGN_SIZE = 30000193;
+    private static final int ALIGNED_ALLOC_SIZE = 30002009;
+
+    private static void doMallocRealloc() {
+        long ptr = Native.malloc(MALLOC_SIZE);
+        long reallocd = Native.realloc(ptr, REALLOC_SIZE);
+        Native.free(reallocd);
+    }
+
+    private static void doCalloc() {
+        long ptr = Native.calloc(1, CALLOC_SIZE);
+        Native.free(ptr);
+    }
+
+    private static void doPosixMemalign() {
+        long ptr = Native.posixMemalign(16, POSIX_MEMALIGN_SIZE);
+        Native.free(ptr);
+    }
+
+    private static void doAlignedAlloc() {
+        long ptr = Native.alignedAlloc(16, ALIGNED_ALLOC_SIZE);
+        Native.free(ptr);
+    }
 
     private static void do_work(boolean once) {
         try {
             do {
-                long addr = Native.malloc(MALLOC_SIZE);
-                long reallocd = Native.realloc(addr, REALLOC_SIZE);
-                Native.free(reallocd);
-
-                long callocd = Native.calloc(1, CALLOC_SIZE);
-                Native.free(callocd);
+                doMallocRealloc();
+                doCalloc();
+                doPosixMemalign();
+                doAlignedAlloc();
 
                 Thread.sleep(1);
             } while (!once);
