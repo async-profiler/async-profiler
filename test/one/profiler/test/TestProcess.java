a0bbc64c48f03693f5597e4c24ccdbc501d763c8
Vishesh Ruparelia <ruparev@amazon.com>
2025-02-24 20:14:46 +0000

    [REVERT_THIS_AND_ONE_BFORE_THIS] increase timeout

--- test/one/profiler/test/TestProcess.java
+++ test/one/profiler/test/TestProcess.java
@@ -314,7 +314,7 @@ public class TestProcess implements Closeable {
                 .redirectError(createTempFile(PROFERR))
                 .start();
 
-        waitForExit(p, 10);
+        waitForExit(p, 60);
         int exitCode = p.waitFor();
         if (exitCode != 0) {
             throw new IOException("Profiling call failed: " + readFile(PROFERR));
