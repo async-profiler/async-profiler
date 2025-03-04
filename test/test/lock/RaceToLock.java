106d01bd8b1e4d3e2a17a3acfda02bc70f097910
Vishesh Ruparelia <ruparev@amazon.com>
2025-03-04 15:31:03 +0000

    reduce duration

--- test/test/lock/RaceToLock.java
+++ test/test/lock/RaceToLock.java
@@ -74,7 +74,7 @@ public class RaceToLock {
             t.start();
         }
 
-        Thread.sleep(10000);
+        Thread.sleep(1);
         app.exitRequested = true;
         for (Thread t : threads) {
             t.join();
