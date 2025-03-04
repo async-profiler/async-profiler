67a8cf2e42d20e25ccc1ceacc9f7644c0d6f7275
Vishesh Ruparelia <ruparev@amazon.com>
2025-03-04 15:15:21 +0000

    increase duration

--- test/test/lock/LockTests.java
+++ test/test/lock/LockTests.java
@@ -14,7 +14,7 @@ public class LockTests {
 
     @Test(mainClass = DatagramTest.class, debugNonSafepoints = true)
     public void datagramSocketLock(TestProcess p) throws Exception {
-        Output out = p.profile("-e cpu -d 10 -o collapsed --cstack dwarf");
+        Output out = p.profile("-e cpu -d 3 -o collapsed --cstack dwarf");
         assert out.ratio("(PlatformEvent::.ark|PlatformEvent::.npark)") > 0.1
                 || ((out.ratio("ReentrantLock.lock") + out.ratio("ReentrantLock.unlock")) > 0.1 && out.contains("Unsafe_.ark"));
         out = p.profile("-e lock -d 3 -o collapsed");
@@ -28,7 +28,7 @@ public class LockTests {
     @Test(mainClass = RaceToLock.class, inputs = "100000000", output = true)
     public void raceToLocks(TestProcess p) throws Exception {
         int interval = Integer.parseInt(p.inputs()[0]);
-        Output out = p.profile("--lock " + interval + " --threads -o collapsed");
+        Output out = p.profile("--lock " + interval + " --threads -o collapsed -d 10");
         Output stdout = p.readFile(TestProcess.STDOUT);
 
         Assert.isGreater(out.samples("\\[random1"), 0, "sampled all threads 1/4");
