f6fdae9ec3cc43c945b0c7fcb891b19deef97d9b
Johannes Bechberger <johannes.bechberger@sap.com>
2025-03-13 09:44:59 +0100

    Warn if load is too high

--- test/test/cpu/CpuTests.java
+++ test/test/cpu/CpuTests.java
@@ -46,9 +46,6 @@ public class CpuTests {
             System.err.println("Therefore the test is skipped");
         }
         long actual = out.total() + 100_000_000; // include the end of the last slice, as the total time is always 100ms less than the actual profiling duration
-        System.out.println("CPU time / wall time ratio: " + ratio);
-        System.out.println("itimer total: " + actual / 1_000_000 + " cpu " + cpuTime / 1_000_000 + " wall " + wallTime / 1_000_000 + " expected " + (long)(2_000 * ratio));
-
         assertCloseTo(actual, (long)(2_000_000_000 * ratio), "itimer total should match CPU time spent in the process during profiling");
     }
 }
