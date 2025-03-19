07cb40f86ab2aa2887eaf062058b44e3305fdd5e
Francesco Andreuzzi <fandreuz@amazon.com>
2025-03-19 08:30:00 +0000

    reduce cpu idx by 1

--- test/test/cpu/CpuTests.java
+++ test/test/cpu/CpuTests.java
@@ -48,15 +48,15 @@ public class CpuTests {
     public void perfEventsWrongTargetCpu(TestProcess p) throws Exception {
         pinCpu(p, 1);
 
-        Output out = p.profile("-d 2 -e cpu -i 100ms --total -o collapsed --target-cpu 2");
+        Output out = p.profile("-d 2 -e cpu -i 100ms --total -o collapsed --target-cpu 0");
         Assert.isEqual(out.total(), 0, "perf_events total should be 0 when the wrong CPU is targeted");
     }
 
     @Test(mainClass = CpuBurner.class, os = Os.LINUX)
     public void perfEventsRightTargetCpu(TestProcess p) throws Exception {
-        pinCpu(p, 1);
+        pinCpu(p, 0);
 
-        Output out = p.profile("-d 2 -e cpu -i 100ms --total -o collapsed --target-cpu 1");
+        Output out = p.profile("-d 2 -e cpu -i 100ms --total -o collapsed --target-cpu 0");
         assertCloseTo(out.total(), 2_000_000_000, "perf_events total should match profiling duration");
     }
 
@@ -64,15 +64,15 @@ public class CpuTests {
     public void perfEventsWrongTargetCpuWithFdTransfer(TestProcess p) throws Exception {
         pinCpu(p, 1);
 
-        Output out = p.profile("-d 2 -e cpu -i 100ms --total -o collapsed --target-cpu 2 --fdtransfer");
+        Output out = p.profile("-d 2 -e cpu -i 100ms --total -o collapsed --target-cpu 0 --fdtransfer");
         Assert.isEqual(out.total(), 0, "perf_events total should be 0 when the wrong CPU is targeted");
     }
 
     @Test(mainClass = CpuBurner.class, os = Os.LINUX)
     public void perfEventsRightTargetCpuWithFdTransfer(TestProcess p) throws Exception {
-        pinCpu(p, 1);
+        pinCpu(p, 0);
 
-        Output out = p.profile("-d 2 -e cpu -i 100ms --total -o collapsed --target-cpu 1 --fdtransfer");
+        Output out = p.profile("-d 2 -e cpu -i 100ms --total -o collapsed --target-cpu 0 --fdtransfer");
         assertCloseTo(out.total(), 2_000_000_000, "perf_events total should match profiling duration");
     }
 
