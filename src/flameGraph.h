a6c441e793e85d639dcdd300994cd12fcec4eb6e
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-03-28 11:45:49 +0000

    Revert "use shared_ptr to reference Trie"
    This reverts commit 9554e70b194a9133fffdb739223db5c3e9c6a492.

--- src/flameGraph.h
+++ src/flameGraph.h
@@ -12,12 +12,11 @@
 #include "arguments.h"
 #include "vmEntry.h"
 #include "writer.h"
-#include <memory>
 
 
 class Trie {
   public:
-    std::map<u32, std::shared_ptr<Trie>> _children;
+    std::map<u32, Trie> _children;
     u64 _total;
     u64 _self;
     u64 _inlined, _c1_compiled, _interpreted;
@@ -41,20 +40,16 @@ class Trie {
         return key & ((1 << 28) - 1);
     }
 
-    std::shared_ptr<Trie> child(u32 name_index, FrameTypeId type) {
-        auto &ptr = _children[name_index | type << 28];
-        if (!ptr) {
-            ptr.reset(new Trie());
-        }
-        return ptr;
+    Trie* child(u32 name_index, FrameTypeId type) {
+        return &_children[name_index | type << 28];
     }
 
     int depth(u64 cutoff, u32* name_order) const {
         int max_depth = 0;
-        for (auto it = _children.begin(); it != _children.end(); ++it) {
-            if (it->second->_total >= cutoff) {
+        for (std::map<u32, Trie>::const_iterator it = _children.begin(); it != _children.end(); ++it) {
+            if (it->second._total >= cutoff) {
                 name_order[nameIndex(it->first)] = 1;
-                int d = it->second->depth(cutoff, name_order);
+                int d = it->second.depth(cutoff, name_order);
                 if (d > max_depth) max_depth = d;
             }
         }
@@ -65,7 +60,7 @@ class Trie {
 
 class FlameGraph {
   private:
-    std::shared_ptr<Trie> _root;
+    Trie _root;
     std::map<std::string, u32> _cpool;
     u32* _name_order;
     u64 _mintotal;
@@ -81,14 +76,14 @@ class FlameGraph {
     u64 _last_x;
     u64 _last_total;
 
-    void printFrame(Writer& out, u32 key, std::shared_ptr<const Trie> f, int level, u64 x);
-    void printTreeFrame(Writer& out, std::shared_ptr<const Trie> f, int level, const char** names);
+    void printFrame(Writer& out, u32 key, const Trie& f, int level, u64 x);
+    void printTreeFrame(Writer& out, const Trie& f, int level, const char** names);
     void printCpool(Writer& out);
     const char* printTill(Writer& out, const char* data, const char* till);
 
   public:
     FlameGraph(const char* title, Counter counter, double minwidth, bool reverse, bool inverted) :
-        _root(std::make_shared<Trie>()),
+        _root(),
         _cpool(),
         _title(title),
         _counter(counter),
@@ -101,11 +96,11 @@ class FlameGraph {
         _buf[sizeof(_buf) - 1] = 0;
     }
 
-    std::shared_ptr<Trie> root() {
-        return _root;
+    Trie* root() {
+        return &_root;
     }
 
-    std::shared_ptr<Trie> addChild(std::shared_ptr<Trie> f, const char* name, FrameTypeId type, u64 value);
+    Trie* addChild(Trie* f, const char* name, FrameTypeId type, u64 value);
 
     void dump(Writer& out, bool tree);
 };
