3bafe308eb96c6d4a0c7816d36b6034f2a75fa16
Andrei Pangin <noreply@pangin.pro>
2025-04-06 19:51:05 +0100

    #1125: Use dlopen instead of dl_iterate_phdr for parsing libraries

--- src/symbols_macos.cpp
+++ src/symbols_macos.cpp
@@ -5,7 +5,7 @@
 
 #ifdef __APPLE__
 
-#include <set>
+#include <unordered_set>
 #include <dlfcn.h>
 #include <string.h>
 #include <mach-o/dyld.h>
@@ -126,7 +126,7 @@ class MachOParser {
 
 Mutex Symbols::_parse_lock;
 bool Symbols::_have_kernel_symbols = false;
-static std::set<const void*> _parsed_libraries;
+static std::unordered_set<const void*> _parsed_libraries;
 
 void Symbols::parseKernelSymbols(CodeCache* cc) {
 }
