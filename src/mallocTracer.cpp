1b776ce6388d66d3d7b1100901f0ff464c0b1b14
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-04-28 14:23:56 +0000

    revert

--- src/mallocTracer.cpp
+++ src/mallocTracer.cpp
@@ -15,7 +15,6 @@
 
 #define ADDRESS_OF(sym) ({               \
     void* addr = dlsym(RTLD_NEXT, #sym); \
-    dlerror();                           \
     addr != NULL ? (sym##_t)addr : sym;  \
 })
 
