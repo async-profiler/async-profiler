e0dd3023d23c26ab473618a7d5502cc1c0c9e9d8
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-05-09 21:09:26 +0000

    rename to protobuf

--- src/protobuf.cpp
+++ src/protobuf.cpp
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "otlp.h"
+#include "protobuf.h"
 #include <string.h>
 
 template <typename T>
--- test/native/protobufBufferTest.cpp
+++ test/native/protobufBufferTest.cpp
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "otlp.h"
+#include "protobuf.h"
 #include "testRunner.hpp"
 #include <cstdint>
 
