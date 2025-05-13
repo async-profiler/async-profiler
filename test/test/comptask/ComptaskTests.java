1baf1647cd8a944e9459a3fbe08afa47c24a0fab
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-05-13 10:43:08 +0000

    cc

--- test/test/comptask/ComptaskTests.java
+++ test/test/comptask/ComptaskTests.java
@@ -11,12 +11,12 @@ public class ComptaskTests {
     @Test(
         mainClass = Main.class, 
         agentArgs = "start,features=comptask,event=Compile::Init", 
-        jvmArgs = "-Xbatch -XX:CompileThreshold=1 -XX:-TieredCompilation -XX:CompileCommand=compileonly,test.comptask.Main::x", 
+        jvmArgs = "-Xbatch -XX:CompileThreshold=1 -XX:-TieredCompilation -XX:CompileCommand=compileonly,test.comptask.Main::toBeCompiled",
         jvm = Jvm.HOTSPOT
     )
     public void testCompTask(TestProcess p) throws Exception {
-        Thread.sleep(3000);
+        Thread.sleep(1500);
         Output out = p.profile("stop -o collapsed");
-        assert out.contains("test/comptask/Main.x;C2Compiler::compile_method");
+        assert out.contains("test/comptask/Main.toBeCompiled;C2Compiler::compile_method");
     }
 }
--- test/test/comptask/Main.java
+++ test/test/comptask/Main.java
@@ -9,12 +9,14 @@ public class Main {
     public static volatile double sink = 0;
 
     public static void main(String[] args) throws Exception {
+        // Wait so the profiler has time to start
         Thread.sleep(1000);
-        x();
-        Thread.sleep(3000);
+        toBeCompiled();
+        // Wait so the profiler has time to stop gracefully
+        Thread.sleep(1000);
     }
 
-    public static void x() throws Exception {
+    public static void toBeCompiled() throws Exception {
         // Simulate some CPU-bound work
         double result = 0;
         for (int j = 0; j < 1000000; j++) {
