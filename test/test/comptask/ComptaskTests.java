e15f02754bc03b0b61c90e15b951fe1028de97a0
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-05-19 10:41:41 +0000

    print

--- test/test/comptask/ComptaskTests.java
+++ test/test/comptask/ComptaskTests.java
@@ -11,12 +11,13 @@ public class ComptaskTests {
     @Test(
         mainClass = Main.class,
         agentArgs = "start,features=comptask,collapsed,file=%f",
-        jvmArgs = "-Xcomp",
+        jvmArgs = "-XX:+PrintCompilation -Xcomp",
         jvm = Jvm.HOTSPOT
     )
     public void testCompTask(TestProcess p) throws Exception {
         Output out = p.waitForExit("%f");
         assert p.exitCode() == 0;
+        System.err.println(out);
         assert out.contains("CompileBroker::invoke_compiler_on_method;test/comptask/Main.main");
     }
 }
