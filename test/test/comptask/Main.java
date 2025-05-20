110fba93d6f92c12bdb81751481bd2e25e8d3a15
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-05-20 14:14:26 +0000

    cc

--- test/test/comptask/Main.java
+++ test/test/comptask/Main.java
@@ -8,12 +8,9 @@ package test.comptask;
 public class Main {
     private static final long RUN_DURATION_MS = 1000;
 
-    // This function should take a long time to compile
     public static void main(String[] args) throws Exception {
         long start = System.nanoTime();
         long end = start + RUN_DURATION_MS * 1_000_000;
-        while (System.nanoTime() < end) {
-            // Do nothing
-        }
+        while (System.nanoTime() < end) {}
     }
 }
