d1fe7dcf0a7e1d56b1bd33877a9d3552805a69bd
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-06-10 09:41:18 +0000

    private naming

--- src/index.h
+++ src/index.h
@@ -14,20 +14,20 @@
 // Keeps track of values seen and their index of occurrence
 class Index {
   private:
-    std::unordered_map<std::string, u32> idx_map;
+    std::unordered_map<std::string, u32> _idx_map;
   
   public:
     u32 recordIndex(std::string value) {
-        return idx_map.insert({value, idx_map.size()}).first->second;
+        return _idx_map.insert({value, _idx_map.size()}).first->second;
     }
 
     u32 size() const {
-        return idx_map.size();
+        return _idx_map.size();
     }
 
     void forEachOrdered(std::function<void(const std::string&)> consumer) const {
-        const std::string* arr[idx_map.size()];
-        for (const auto& it : idx_map) {
+        const std::string* arr[_idx_map.size()];
+        for (const auto& it : _idx_map) {
             arr[it.second] = &it.first;
         }
         for (u32 idx = 0; idx < size(); ++idx) {
