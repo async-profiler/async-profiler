cf943706aaaf8e4c195f32443f74663e7881e4c3
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-07-11 20:04:27 +0100

    cc

--- src/converter/one/heatmap/MethodCache.java
+++ src/converter/one/heatmap/MethodCache.java
@@ -39,7 +39,8 @@ public class MethodCache {
             if (frameDesc == null) {
                 frameDesc = createMethod(methodId, location, type, firstInStack);
                 nearCache[mid] = frameDesc;
-                return frameDesc.index = methodIndex.index(frameDesc);
+                frameDesc.index = methodIndex.index(frameDesc)
+                return frameDesc.index;
             }
         } else {
             // this should be extremely rare case
