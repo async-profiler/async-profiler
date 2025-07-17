52af9cc2ac0d519159ef81cd07afa0bc0095f3d7
Soumadipta Roy <roysouma@amazon.com>
2025-07-17 14:19:55 +0100

    Changes to launcher for full posix compliance.

--- src/launcher/launcher.sh
+++ src/launcher/launcher.sh
@@ -3,22 +3,28 @@
 # Copyright The async-profiler authors
 # SPDX-License-Identifier: Apache-2.0
 
-if [[ "$1" == "-v" || "$1" == "--version" ]]; then
+if [ "$1" = "-v" ] || [ "$1" = "--version" ]; then
     echo "JFR converter PROFILER_VERSION built on BUILD_DATE"
     exit 0
 fi
 
-JAVA_CMD=("-Xss2M" "-Dsun.misc.URLClassPath.disableJarChecking")
+JAVA_OPTS="-Xss2M -Dsun.misc.URLClassPath.disableJarChecking"
 
-while [[ $# -gt 0 ]]; do
+while [ $# -gt 0 ]; do
     case "$1" in
-        -D*|-X*|-agent*)
-            if [[ ${#1} -gt 2 ]]; then
-                JAVA_CMD+=("$1")
+        -D*|-X*)
+            if [ ${#1} -gt 2 ]; then
+                JAVA_OPTS="$JAVA_OPTS $1"
+            fi
+            ;;
+        -agent*)
+            if [ "$(echo "$1" | cut -c1-6)" = "-agent" ]; then
+                JAVA_OPTS="$JAVA_OPTS $1"
             fi
             ;;
         -J*)
-            JAVA_CMD+=("${1:2}")
+            opt=$(echo "$1" | cut -c3-)
+            JAVA_OPTS="$JAVA_OPTS $opt"
             ;;
         *)
             break
@@ -27,25 +33,23 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
-JAVA_CMD+=("-jar" "$0" "$@")
-
 # 1. Try JAVA_HOME
-if [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]]; then
-    exec "$JAVA_HOME/bin/java" "${JAVA_CMD[@]}"
+if [ -n "$JAVA_HOME" ] && [ -x "$JAVA_HOME/bin/java" ]; then
+    exec "$JAVA_HOME/bin/java" $JAVA_OPTS -jar "$0" "$@"
 fi
 
 # 2. Try PATH
 if command -v java >/dev/null 2>&1; then
-    exec java "${JAVA_CMD[@]}"
+    exec java $JAVA_OPTS -jar "$0" "$@"
 fi
 
 # 3. Try /etc/alternatives/java
-if [[ -x "/etc/alternatives/java" ]]; then
-    exec "/etc/alternatives/java" "${JAVA_CMD[@]}"
+if [ -x "/etc/alternatives/java" ]; then
+    exec "/etc/alternatives/java" $JAVA_OPTS -jar "$0" "$@"
 fi
 
 # 4. Try common JVM directories
-if [[ "$(uname -s)" == "darwin"* ]]; then
+if [ "$(uname -s)" = "Darwin" ]; then
     JVM_DIR="/Library/Java/JavaVirtualMachines"
     CONTENTS_HOME="/Contents/Home"
 else
@@ -53,12 +57,12 @@ else
     CONTENTS_HOME=""
 fi
 
-if [[ -d "$JVM_DIR" ]]; then
-    for jvm in "$JVM_DIR"/*; do
-        if [[ -d "$jvm" ]]; then
-            java_exe="$jvm$CONTENTS_HOME/bin/java"
-            if [[ -x "$java_exe" ]]; then
-                exec "$java_exe" "${JAVA_CMD[@]}"
+if [ -d "$JVM_DIR" ]; then
+    for JVM in "$JVM_DIR"/*; do
+        if [ -d "$JVM" ]; then
+            JAVA_EXE="$JVM$CONTENTS_HOME/bin/java"
+            if [ -x "$JAVA_EXE" ]; then
+                exec "$JAVA_EXE" $JAVA_OPTS -jar "$0" "$@"
             fi
         fi
     done
