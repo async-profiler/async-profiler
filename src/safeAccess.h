545cb3d4cb1224651130014ac0f02c7f5fed59db
Andrei Pangin <noreply@pangin.pro>
2025-08-04 00:19:11 +0100

    nullptr

--- src/safeAccess.h
+++ src/safeAccess.h
@@ -10,7 +10,7 @@
 
 class SafeAccess {
   public:
-    static void* load(void** ptr, void* default_value = NULL);
+    static void* load(void** ptr, void* default_value = nullptr);
     static u32 load32(u32* ptr, u32 default_value = 0);
 
     static u32 skipLoad(instruction_t* pc);
