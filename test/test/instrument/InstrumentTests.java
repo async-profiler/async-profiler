d7d261d4e89d7ae7797242954730b7e7d1f6bdf3
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-08-06 14:49:22 +0000

    nn

--- test/test/instrument/InstrumentTests.java
+++ test/test/instrument/InstrumentTests.java
@@ -11,7 +11,6 @@ public class InstrumentTests {
     public void testLatency(TestProcess p) throws Exception {
         Output out = p.waitForExit("%f");
         assert p.exitCode() == 0;
-        System.err.println(out);
 
         assert out.samples("\\[thread1 .*;test\\/instrument\\/CpuBurner\\.lambda\\$main\\$0;test\\/instrument\\/CpuBurner\\.burn") == 1;
         assert out.samples("\\[thread2 .*;test\\/instrument\\/CpuBurner\\.lambda\\$main\\$1;test\\/instrument\\/CpuBurner\\.burn") == 2;
