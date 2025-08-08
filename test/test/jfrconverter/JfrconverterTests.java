d768a3f6a23335b963e7756241ed44dc64fc72b9
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-08-08 16:17:05 +0000

    all

--- test/test/jfrconverter/JfrconverterTests.java
+++ test/test/jfrconverter/JfrconverterTests.java
@@ -14,21 +14,21 @@ public class JfrconverterTests {
 
     private static final String TMP_DIR = System.getProperty("java.io.tmpdir");
 
-    @Test(mainClass = CpuBurner.class, agentArgs = "start,jfr,file=%f")
+    @Test(mainClass = CpuBurner.class, agentArgs = "start,jfr,all,file=%f")
     public void testAllocationHeatmapConversion(TestProcess p) throws Exception {
         Output out = p.waitForExit("%f");
         assert p.exitCode() == 0;
         JfrToHeatmap.convert(p.getFilePath("%f"), "/dev/null", new Arguments("--alloc"));
     }
 
-    @Test(mainClass = CpuBurner.class, agentArgs = "start,jfr,file=%f")
+    @Test(mainClass = CpuBurner.class, agentArgs = "start,jfr,all,file=%f")
     public void testCpuHeatmapConversion(TestProcess p) throws Exception {
         Output out = p.waitForExit("%f");
         assert p.exitCode() == 0;
         JfrToHeatmap.convert(p.getFilePath("%f"), "/dev/null", new Arguments("--cpu"));
     }
 
-    @Test(mainClass = CpuBurner.class, agentArgs = "start,jfr,file=%f")
+    @Test(mainClass = CpuBurner.class, agentArgs = "start,jfr,all,file=%f")
     public void testFlamegraphConversion(TestProcess p) throws Exception {
         Output out = p.waitForExit("%f");
         assert p.exitCode() == 0;
