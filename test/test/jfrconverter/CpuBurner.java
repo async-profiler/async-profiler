bf304f10333ad9f787374870c1d815f132ddb932
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-08-08 15:52:41 +0000

    tests

---
+++ test/test/jfrconverter/CpuBurner.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright The async-profiler authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package test.jfrconverter;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Random;
+
+public class CpuBurner {
+    private static final Random RANDOM = new Random();
+    private static final Duration TEST_DURATION = Duration.ofSeconds(1);
+
+    static void burn() {
+        long n = RANDOM.nextLong();
+        if (Long.toString(n).hashCode() == 0) {
+            System.out.println(n);
+        }
+    }
+
+    public static void main(String[] args) {
+        Instant start = Instant.now();
+        while (Duration.between(start, Instant.now()).compareTo(TEST_DURATION) < 0) {
+            burn();
+        }
+    }
+}
---
+++ test/test/jfrconverter/JfrconverterTests.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright The async-profiler authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package test.jfrconverter;
+
+import one.convert.*;
+import one.profiler.test.*;
+
+// Simple smoke tests for JFR converter. The output is not inspected for errors,
+// we only verify that the conversion completes successfully.
+public class JfrconverterTests {
+
+    private static final String TMP_DIR = System.getProperty("java.io.tmpdir");
+
+    @Test(mainClass = CpuBurner.class, agentArgs = "start,jfr,file=%f")
+    public void testAllocationHeatmapConversion(TestProcess p) throws Exception {
+        Output out = p.waitForExit("%f");
+        assert p.exitCode() == 0;
+        JfrToHeatmap.convert(p.getFilePath("%f"), "/dev/null", new Arguments("alloc"));
+    }
+
+    @Test(mainClass = CpuBurner.class, agentArgs = "start,jfr,file=%f")
+    public void testCpuHeatmapConversion(TestProcess p) throws Exception {
+        Output out = p.waitForExit("%f");
+        assert p.exitCode() == 0;
+        JfrToHeatmap.convert(p.getFilePath("%f"), "/dev/null", new Arguments("cpu"));
+    }
+
+    @Test(mainClass = CpuBurner.class, agentArgs = "start,jfr,file=%f")
+    public void testFlamegraphConversion(TestProcess p) throws Exception {
+        Output out = p.waitForExit("%f");
+        assert p.exitCode() == 0;
+        JfrToFlame.convert(p.getFilePath("%f"), "/dev/null", new Arguments());
+    }
+}
