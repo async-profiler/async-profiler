bfd7d407f6b5ed2b41b0611eb27602007df93fa3
Bara' Hasheesh <bara.hasheesh@gmail.com>
2025-09-05 08:06:51 +0100

    Reduce resonance between methods in CPU smoke test

--- test/test/smoke/Cpu.java
+++ test/test/smoke/Cpu.java
@@ -11,13 +11,13 @@ public class Cpu {
     private static volatile int value;
 
     private static void method1() {
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 314159; i++) {
             value++;
         }
     }
 
     private static void method2() {
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 271828; i++) {
             value++;
         }
     }
