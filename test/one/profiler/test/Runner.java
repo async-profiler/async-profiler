3e663759da655857ab753150c6249e79ce693eb9
Soumadipta Roy <133005763+roy-soumadipta@users.noreply.github.com>
2025-10-02 17:41:06 +0100

    #1510: Add option to retry tests with parameterized retry count (#1520)

--- test/one/profiler/test/Runner.java
+++ test/one/profiler/test/Runner.java
@@ -179,6 +179,8 @@ public class Runner {
         TestDeclaration decl = TestDeclaration.parse(args);
         List<RunnableTest> allTests = decl.getRunnableTests();
         final int testCount = allTests.size();
+        final int retryCount = Integer.parseInt(System.getProperty("retryCount", "0"));
+
         int i = 1;
         long totalTestDuration = 0;
         List<String> failedTests = new ArrayList<>();
@@ -186,6 +188,12 @@ public class Runner {
         for (RunnableTest rt : allTests) {
             long start = System.nanoTime();
             TestResult result = run(rt, decl);
+
+            while (result.status() == TestStatus.FAIL && retryCount-- > 0) {
+                log.log(Level.WARNING, "Test failed, retrying...");
+                result = run(rt, decl);
+            }
+
             long durationNs = System.nanoTime() - start;
 
             totalTestDuration += durationNs;
