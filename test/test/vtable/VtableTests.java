e1ba855cf558cc6a862865cb850e71d77f7bed20
Andrei Pangin <noreply@pangin.pro>
2025-10-15 21:06:06 +0100

    Use VMStructs stack walking mode by default

--- test/test/vtable/VtableTests.java
+++ test/test/vtable/VtableTests.java
@@ -13,14 +13,14 @@ public class VtableTests {
 
     @Test(mainClass = Numbers.class, jvm = Jvm.HOTSPOT)
     public void vtableStubs(TestProcess p) throws Exception {
-        Output out = p.profile("-d 3 -e cpu -i 1ms --cstack vm -F vtable -o collapsed");
+        Output out = p.profile("-d 3 -e cpu -i 1ms -F vtable -o collapsed");
         assert out.contains("Numbers.avg;vtable stub;java.lang.Integer_\\[i]");
         assert out.contains("Numbers.avg;vtable stub;java.lang.Long_\\[i]");
     }
 
     @Test(mainClass = Suppliers.class, jvm = Jvm.HOTSPOT)
     public void itableStubs(TestProcess p) throws Exception {
-        Output out = p.profile("-d 3 -e cpu -i 1ms --cstack vm -F comptask,vtable -o collapsed");
+        Output out = p.profile("-d 3 -e cpu -i 1ms -F comptask,vtable -o collapsed");
         assert out.contains("Suppliers.loop;itable stub;test.recovery.Suppliers[^_]+_\\[i]");
     }
 }
