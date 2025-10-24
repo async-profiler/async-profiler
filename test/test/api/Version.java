f0cd4db5be936533d5404646386dcdfaee84a6c8
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-10-24 16:22:58 +0000

    test

---
+++ test/test/api/Version.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright The async-profiler authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package test.api;
+
+import one.profiler.AsyncProfiler;
+import one.profiler.Events;
+
+public class Version {
+    public static void main(String[] args) throws Exception {
+        System.out.println(AsyncProfiler.getInstance().getVersion());
+    }
+}
