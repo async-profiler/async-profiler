3e0026332a8f4556a593b358712917b0898008b4
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-12-15 11:40:10 +0000

    docs

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -128,6 +128,7 @@ public class Main {
                 "     --dot              Dotted class names\n" +
                 "     --from TIME        Start time in ms (absolute or relative)\n" +
                 "     --to TIME          End time in ms (absolute or relative)\n" +
+                "     --latency L        Retain only events recorded during a jdk.MethodTrace with duration at least L ms\n" +
                 "\n" +
                 "Flame Graph options:\n" +
                 "     --title STRING     Flame Graph title\n" +
