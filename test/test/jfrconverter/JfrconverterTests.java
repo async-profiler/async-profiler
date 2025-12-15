9078804547a0752cf937b8b437fe43ae6a4f7157
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-12-15 12:38:01 +0000

    test

--- test/test/jfrconverter/JfrconverterTests.java
+++ test/test/jfrconverter/JfrconverterTests.java
@@ -7,6 +7,10 @@ package test.jfrconverter;
 
 import test.otlp.CpuBurner;
 import one.convert.*;
+import one.jfr.JfrReader;
+import one.jfr.event.Event;
+import one.jfr.event.EventCollector;
+import one.jfr.StackTrace;
 import one.profiler.test.*;
 
 // Simple smoke tests for JFR converter. The output is not inspected for errors,
@@ -27,5 +31,41 @@ public class JfrconverterTests {
         assert p.exitCode() == 0;
         JfrToFlame.convert(p.getFilePath("%f"), "/dev/null", new Arguments());
         JfrToFlame.convert(p.getFilePath("%f"), "/dev/null", new Arguments("--alloc"));
+    }
+
+    @Test(mainClass = Tracer.class, agentArgs = "start,jfr,wall,trace=test.jfrconverter.Tracer.traceMethod,file=%f")
+    public void latencyFilter(TestProcess p) throws Exception {
+        Output out = p.waitForExit("%f");
+        assert p.exitCode() == 0;
+
+        try (JfrReader jfr = new JfrReader(p.getFilePath("%f"))) {
+            boolean[] foundShowcase = new boolean[3];
+            JfrConverter converter = new JfrConverter(jfr, new Arguments("--wall", "--latency", Tracer.TRACE_DURATION_MS + "")) {
+                protected void convertChunk() {
+                    collector.forEach(new EventCollector.Visitor() {
+                        public void visit(Event event, long samples, long value) {
+                            StackTrace stackTrace = jfr.stackTraces.get(event.stackTraceId);
+                            if (stackTrace == null) return;
+
+                            long[] methods = stackTrace.methods;
+                            byte[] types = stackTrace.types;
+                            for (int i = methods.length; --i >= 0; ) {
+                                String methodName = getMethodName(methods[i], types[i]);
+                                if (!methodName.startsWith("test/jfrconverter/Tracer.showcase")) continue;
+
+                                int idx = Integer.parseInt(methodName.charAt(methodName.length() - 1) + "");
+                                foundShowcase[idx - 1] = true;
+                                break;
+                            }
+                        }
+                    });
+                }
+            };
+            converter.convert();
+
+            assert foundShowcase[0];
+            assert foundShowcase[1];
+            assert !foundShowcase[2];
+        }
     }
 }
