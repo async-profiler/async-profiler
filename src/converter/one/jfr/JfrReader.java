2c5aecb48b52c2d5303bfe633b4c5785dd25c00c
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-12-16 17:06:35 +0000

    nn

--- src/converter/one/jfr/JfrReader.java
+++ src/converter/one/jfr/JfrReader.java
@@ -20,7 +20,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Parses JFR output produced by async-profiler.
