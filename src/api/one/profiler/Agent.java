5cf09a5c0e147c3314c0c89116864837936eac8f
Andrew Azores <aazores@ibm.com>
2025-12-20 12:48:54 -0500

    general throw Exception

--- src/api/one/profiler/Agent.java
+++ src/api/one/profiler/Agent.java
@@ -5,34 +5,17 @@
 
 package one.profiler;
 
-import java.io.IOException;
 import java.lang.management.ManagementFactory;
 
-import javax.management.InstanceAlreadyExistsException;
-import javax.management.MBeanRegistrationException;
-import javax.management.MalformedObjectNameException;
-import javax.management.NotCompliantMBeanException;
 import javax.management.ObjectName;
 
 public class Agent {
 
-    public static void premain(String args) throws InstanceAlreadyExistsException,
-           MBeanRegistrationException,
-           NotCompliantMBeanException,
-           MalformedObjectNameException,
-           IllegalArgumentException,
-           IllegalStateException,
-           IOException {
+    public static void premain(String args) throws Exception {
         agentmain(args);
     }
 
-    public static void agentmain(String args) throws InstanceAlreadyExistsException,
-           MBeanRegistrationException,
-           NotCompliantMBeanException,
-           MalformedObjectNameException,
-           IllegalArgumentException,
-           IllegalStateException,
-           IOException {
+    public static void agentmain(String args) throws Exception {
         AsyncProfiler instance = AsyncProfiler.getInstance();
             ManagementFactory.getPlatformMBeanServer().registerMBean(
                     instance,
