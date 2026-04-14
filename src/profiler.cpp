8aab346c3b95e41bace43d5a6811f92309769fb0
Andrei Pangin <noreply@pangin.pro>
2026-04-14 15:55:46 +0100

    #1727: Allocation profile has wrong units in OTLP format

--- src/profiler.cpp
+++ src/profiler.cpp
@@ -1398,7 +1398,7 @@ void Profiler::dumpText(Writer& out, Arguments& args) {
 
 void Profiler::dumpOtlp(Writer& out, Arguments& args) {
     FrameName fn(args, args._style & ~STYLE_ANNOTATE, _epoch, _thread_names_lock, _thread_names);
-    Otlp::Recorder recorder(_engine, fn, _start_time * 1000ULL, (OS::micros() - _start_time) * 1000ULL);
+    Otlp::Recorder recorder(activeEngine(), fn, _start_time * 1000ULL, (OS::micros() - _start_time) * 1000ULL);
     std::vector<CallTraceSample*> call_trace_samples;
     _call_trace_storage.collectSamples(call_trace_samples);
     recorder.record(call_trace_samples, args._counter == COUNTER_SAMPLES);
